69ee86858d7af140e792b6bddb31da9bbf5124ac
adithyawn <adithyawn.learning@gmail.com>
2022-02-05 23:48:34 +0700

    status code

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+# import Movie model to be used in create method
+from watchlist_app.models import Movie
 
 # if Model is Movie then serializer is MovieSerializer
 class MovieSerializer(serializers.Serializer):
@@ -8,9 +10,55 @@ class MovieSerializer(serializers.Serializer):
     description = serializers.CharField()
     active = serializers.BooleanField()
 
+    # to use post request, Crate Instance Method with name def create. Will return validated data to views with structure like in Movie model db
+    def create(self,validated_data):
+        return Movie.objects.create(**validated_data)
 
-# AttributeError: Got AttributeError when attempting to get a value for field `name` on serializer `MovieSerializer`.
-# The serializer field might be named incorrectly and not match any attribute or key on the `QuerySet` instance.
-# Original exception text was: 'QuerySet' object has no attribute 'name'.
-# Do one thing, delete current values for the movie from the admin and re-add them.
-# This error is occurring due to multiple values in response when only 1 was requested.
+    # to use put request, Crate Instance Method with name def update. instance will carry old values and validated_data will return validated data to views (new value)
+    def update(self, instance,validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
+
+
+# '''
+
+# # Python program to demonstrate
+# # instance methods
+  
+  
+# class shape:
+      
+#     # Calling Constructor
+#     def __init__(self, edge, color):
+#         self.edge = edge
+#         self.color = color
+          
+#     # Instance Method
+#     def finEdges(self):
+#         return self.edge
+          
+#     # Instance Method
+#     def modifyEdges(self, newedge):
+#         self.edge = newedge
+          
+# # Driver Code
+# circle = shape(0, 'red')
+# square = shape(4, 'blue')
+  
+# # Calling Instance Method
+# print("No. of edges for circle: "+ str(circle.finEdges()))
+  
+# # Calling Instance Method
+# square.modifyEdges(6)
+  
+# print("No. of edges for square: "+ str(square.finEdges()))
+
+# # Output
+
+# # No. of edges for circle: 0
+# # No. of edges for square: 6
+
+# '''
--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,26 +1,78 @@
+from email import message
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
-@api_view()
+@api_view(["GET","POST"])
 def movie_list(request):
-    movies = Movie.objects.all()
-    # print("movies :", movies)
-    # movies return <QuerySet [<Object1>, <Object2>,..]>,  And we are trying to serialize the list as a single object. Use many=True tells the serializer that a list of objects being passed for serialization.
-    serializer = MovieSerializer(movies, many=True)
-    # print("serializer :", serializer.data[1]['name']) output : Starwars Mandalorian
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        # print("movies :", movies)
+        # movies return <QuerySet [<Object1>, <Object2>,..]>,  And we are trying to serialize the list as a single object. Use many=True tells the serializer that a list of objects being passed for serialization.
+        
+        # How Serializer Works? Convert QuerySet in movies DB to JSON but validate first with MovieSerializer
+        serializer = MovieSerializer(movies, many=True)
 
-    return Response(serializer.data)
+        # print("serializer :", serializer.data[1]['name']) output : Starwars Mandalorian
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        # Get requested data from input form then Serializer it to MovieSerializer
+        serializer = MovieSerializer(data=request.data)
+        # print("serializer : ",serializer) , output : MovieSerializer(data={'name': 'Mandor Harian', 'description': 'About Mandor', 'active': False})
+
+        # if valid then save to serializer & DB 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 # DoesNotExist at /movie/1
 # Movie matching query does not exist. If parameters passed None it will be error. Example there is no id = 1
 
-@api_view()
+@api_view(['GET','PUT','DELETE'])
 def movie_details(request,id):
-    movie = Movie.objects.get(id=id)
-    # Because will return only 1 QuerySet by id so doesnt need many=True
-    serializer = MovieSerializer(movie)
-    # print("serializer :", serializer.data['name']) output : Starwars Mandalorian
 
-    return Response(serializer.data)
+    if request.method == 'GET':
+        try:
+            movie = Movie.objects.get(id=id)
+            # print("movie : ", movie) output will return Class of <Object1>
+        except Movie.DoesNotExist:
+            # Because will return only 1 QuerySet by id so doesnt need many=True in serializer
+            # How Serializer Works? Convert Class of <Object1> in movie DB to JSON but validate first with MovieSerializer
+            return Response({"message":"Movie Not Found !"},status=status.HTTP_404_NOT_FOUND)
+
+        serializer = MovieSerializer(movie)
+        # print("serializer : ",serializer) , ouput : MovieSerializer(<Movie: Star Wars Mandalorian>)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        try:
+            movie = Movie.objects.get(id=id)
+        except Movie.DoesNotExist:
+            return Response({"message":"Movie Not Found !"},status=status.HTTP_404_NOT_FOUND)
+
+        # Convert and validate updated data with serializer. Then update movie DB (spesific id) with that data. Remember def update(self, instance,validated_data) ! movie is current class DB which will pass instance & data is updated data given from form / input which will pass validated_data
+        serializer = MovieSerializer(movie, data=request.data)
+
+        # print("serializer : ",serializer) , output : MovieSerializer(data={'name': 'Mandor Harian', 'description': 'About Mandor', 'active': False})
+        #  if valid then save
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            # return error if format not valid
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        # in DELETE doesnt need Serializer because it dont need to return json
+        try :
+            movie = Movie.objects.get(id=id)
+        except Movie.DoesNotExist:
+            return Response({"message":"Movie Not Found !"},status=status.HTTP_404_NOT_FOUND)
+        movie.delete()
+        return Response({"message":"Movie has been deleted !"})
