43cc57586ae12dc7bc91b3d0769d0edeb3b9fe3d
adithyawn <adithyawn.learning@gmail.com>
2022-02-08 11:00:01 +0700

    done ModelSerializer

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -2,56 +2,79 @@ from rest_framework import serializers
 # import Movie model to be used in create method
 from watchlist_app.models import Movie
 
-# validation function
-def name_length(value):
-    if len(value) < 2:
-        # return exception
-        raise serializers.ValidationError("Name is too short")
+# If dont want to declare variable / field again (reuse from Movie model (from watchlist_app.models import Movie)) can use (serializers.ModelSerializer). It's diffent with (serializers.Serializer)
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        # fields = "__all__"
+        # If want spesific field. It will only show {"id":1,"name":TEST} without desc and active. If you post name, desc automaticaly will be "" and active will be true
+        # fields = ['id','name']
 
-# if Model is Movie then serializer is MovieSerializer
-class MovieSerializer(serializers.Serializer):
-    # read only so id cant be edited and generated automatically
-    id = serializers.IntegerField(read_only=True)
-    # validation with function
-    name = serializers.CharField(validators=[name_length])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+        # Also can use exclude so it will be same like fields= ['id','name']. But fields must be empty / deleted
+        exclude = ['description','active']
 
-    # to use post request, Crate Instance Method with name def create. Will return validated data to views with structure like in Movie model db
-    def create(self,validated_data):
-        return Movie.objects.create(**validated_data)
-
-    # to use put request, Crate Instance Method with name def update. instance will carry old values and validated_data will return validated data to views (new value)
-    def update(self, instance,validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
-
-# Field Level Validation https://www.django-rest-framework.org/api-guide/serializers/#field-level-validation
-    # def validate_{obj_name}(self,value)
-    # def validate_name(self,value):
-    #     if len(value) < 2:
-    #         # return exception
-    #         raise serializers.ValidationError("Name is too short")
-    #     else:
-    #         return value
-    def validate_description(self,value):
-        if len(value) < 2:
+    # validation method
+    def validate_name(self,data):
+        if len(data) < 2:
             # return exception
-            raise serializers.ValidationError("Description is too short")
+            raise serializers.ValidationError("Name is too short")
         else:
-            return value
+            return data    
 
-# Object Level Validation https://www.django-rest-framework.org/api-guide/serializers/#object-level-validation
-    def validate(self,data):
-        if data['name'] == data['description']:
-            raise serializers.ValidationError("Name and Description should be different")
-        else:
-            return data
+# '''
+# # validation function
+# def name_length(data):
+#     if len(data) < 2:
+#         # return exception
+#         raise serializers.ValidationError("Name is too short")
 
+# # if Model is Movie then serializer is MovieSerializer
+# class MovieSerializer(serializers.Serializer):
+#     # read only so id cant be edited and generated automatically. read_only, validators are Core Arguments https://www.django-rest-framework.org/api-guide/fields/#core-arguments
+#     # Serializer fields (Ex: IntegerField, CharField, EmailField etc.. ) handle converting between primitive values and internal datatypes. They also deal with validating input values, as well as retrieving and setting the values from their parent objects. 
+#     # Note: The serializer fields are declared in fields.py, but by convention you should import them using from rest_framework import serializers and refer to fields as serializers.<FieldName>.
 
+#     id = serializers.IntegerField(read_only=True)
+#     # validation with function
+#     name = serializers.CharField(validators=[name_length])
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+
+#     # to use post request, Crate Instance Method with name def create. Will return validated data to views with structure like in Movie model db
+#     def create(self,validated_data):
+#         return Movie.objects.create(**validated_data)
+
+#     # to use put request, Crate Instance Method with name def update. instance will carry old values and validated_data will return validated data to views (new value)
+#     def update(self, instance,validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+
+# # Field Level Validation https://www.django-rest-framework.org/api-guide/serializers/#field-level-validation
+#     # def validate_{obj_name}(self,data)
+#     # def validate_name(self,data):
+#     #     if len(data) < 2:
+#     #         # return exception
+#     #         raise serializers.ValidationError("Name is too short")
+#     #     else:
+#     #         return data
+#     def validate_description(self,data):
+#         if len(data) < 2:
+#             # return exception
+#             raise serializers.ValidationError("Description is too short")
+#         else:
+#             return data
+
+# # Object Level Validation https://www.django-rest-framework.org/api-guide/serializers/#object-level-validation
+#     def validate(self,data):
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError("Name and Description should be different")
+#         else:
+#             return data
+
+# '''
 
 
 
